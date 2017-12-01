b84c73ce21eb6780e9c49098696f4d370887f380
Eugenio <eyakhnenko@8amarketing.com>
2017-12-01 15:41:07 -0300

    estructura motor sonido

---
+++ src/sounds.js
@@ -0,0 +1,53 @@
+class Sound extends Component{
+	constructor(params, engine){
+		super(params, engine);
+		this.extra = 'extra algo';
+	}
+	init(){
+		// llamado cuando el componente es agregado al motor, aqui se podrian cargar los sonidos tal vez?
+	}
+	move(){
+		// se ejecuta cada ciclo del gameloop
+		// podria estar vacio
+	}
+	draw(){
+		// se ejecuta cada ciclo del gameloop
+		// podria estar vacio
+	}
+
+
+	load(src){
+
+	}
+	play(name){
+
+	}
+	stop(name){
+
+	}
+	pause(name){
+
+	}
+
+	/**
+	 * todo: cualquier cosa que se ocurra, sonidos podrian loopear, otros no.
+	 * Musica de fondo seria un sonido?
+	 */
+}
+
+/**
+ * Metodo de testeo sin agregar al motor
+ * el primer parametro es un object literal 
+ * que puede tener lo que sea que se necesite cuando se contruye
+ */
+// let sound = new Sound({}, null);
+// ej2
+// let sound = new Sound({algo: "algo"}, null);
+
+/**
+ * metodo de testeo en el motor
+ * engine.addComponent("Sound", Sound, {params});
+ *
+ * luego desde cualquier sprite o componente
+ * this.sound = this.getComponent("Sound"); //devuelve la instancia del motor de sonido del motor.
+ */
